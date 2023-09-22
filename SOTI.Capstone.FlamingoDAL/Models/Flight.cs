74ab5fe0cd11c8d6e090edc9808813db8df5a2d8
FABhishek <abhishekasc3@gmail.com>
2023-09-22 12:57:41 +0530

    firstCommit

---
+++ SOTI.Capstone.FlamingoDAL/Models/Flight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTI.Capstone.FlamingoDAL.Models
+{
+    internal class Flight
+    {
+    }
+}
