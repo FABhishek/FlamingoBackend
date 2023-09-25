e942e112fc4befcb97ab3717dd3652ab2e5abb97
Snehasharma76 <snehasharma7620@gmail.com>
2023-09-25 14:08:12 +0530

    Added MODELS/CONTROLLERS

---
+++ SOTI.Capstone.FlamingoDAL/Models/BookingCancellation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTI.Capstone.FlamingoDAL.Models
+{
+    public class BookingCancellation
+    {
+        public int PNRNo { get; set; }
+        public int FlightId { get; set; }
+        public char RefundStatus { get; set; }
+        public int RefundAmount { get; set; }
+
+    }
+}
